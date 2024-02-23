c7d3c926871729bedb2b72b6cea530746c2ff060
Runze Li <lirunze.me@gmail.com>
2024-02-23 16:43:36 -0500

    display keypresses

--- kilo.c
+++ kilo.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <termios.h>
@@ -12,7 +14,15 @@ int main() {
 	enableRawMode();
 	
 	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+		if (iscntrl(c)) {
+			printf("%d\n", c);
+		}
+		else {
+			printf("%d ('%c')\n", c, c);
+		}
+	}
+
 	return 0;
 
 }
