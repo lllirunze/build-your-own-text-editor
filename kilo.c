0c5660f42ddaeffc49778ce40b7c39b411b2932c
Runze Li <lirunze.me@gmail.com>
2024-02-27 20:10:38 -0500

    status message

--- kilo.c
+++ kilo.c
@@ -5,14 +5,16 @@
 #define _GNU_SOURCE
 
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <termios.h>
-#include <errno.h>
+#include <time.h>
+#include <unistd.h>
 
 /*** defines ***/
 
@@ -52,6 +54,8 @@ struct editorConfig {
 	int numrows;
 	erow *row;
 	char *filename;
+	char statusmsg[80];
+	time_t statusmsg_time;
 	struct termios orig_termios;
 };
 
@@ -91,7 +95,9 @@ void abFree(struct abuf *ab);
 void editorScroll();
 void editorDrawRows(struct abuf *ab);
 void editorDrawStatusBar(struct abuf *ab);
+void editorDrawMessageBar(struct abuf *ab);
 void editorRefreshScreen();
+void editorSetStatusMessage(const char *fmt, ...);
 
 /*** input ***/
 
@@ -109,6 +115,8 @@ int main(int argc, char *argv[]) {
 	if (argc >= 2) {
 		editorOpen(argv[1]);
 	}
+
+	editorSetStatusMessage("HELP: Ctrl-Q = quit");
 	
 	while (1) {
 		editorRefreshScreen();
@@ -463,6 +471,20 @@ void editorDrawStatusBar(struct abuf *ab) {
 		}
 	}
 	abAppend(ab, "\x1b[m", 3);
+	abAppend(ab, "\r\n", 2);
+
+}
+
+void editorDrawMessageBar(struct abuf *ab) {
+
+	abAppend(ab, "\x1b[K", 3);
+	int msglen = strlen(E.statusmsg);
+	if (msglen > E.screencols) {
+		msglen = E.screencols;
+	}
+	if (msglen && time(NULL) - E.statusmsg_time < 5) {
+		abAppend(ab, E.statusmsg, msglen);
+	}
 
 }
 
@@ -478,6 +500,7 @@ void editorRefreshScreen() {
 
 	editorDrawRows(&ab);
 	editorDrawStatusBar(&ab);
+	editorDrawMessageBar(&ab);
 
 	char buf[32];
 	// snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
@@ -489,6 +512,16 @@ void editorRefreshScreen() {
 
 	write(STDOUT_FILENO, ab.b, ab.len);
 	abFree(&ab);
+
+}
+
+void editorSetStatusMessage(const char *fmt, ...) {
+
+	va_list ap;
+	va_start(ap, fmt);
+	vsnprintf(E.statusmsg, sizeof(E.statusmsg), fmt, ap);
+	va_end(ap);
+	E.statusmsg_time = time(NULL);
 
 }
 
@@ -593,11 +626,13 @@ void initEditor() {
 	E.numrows = 0;
 	E.row = NULL;
 	E.filename = NULL;
+	E.statusmsg[0] = '\0';
+	E.statusmsg_time = 0;
 
 	if (getWindowSize(&E.screenrows, &E.screencols) == -1) {
 		die("getWindowSize");
 	}
-	E.screenrows -= 1;
+	E.screenrows -= 2;
 
 }
 
